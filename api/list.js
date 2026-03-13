09454cac916851fce77afd15b82a113839874c9c
lorpsum <valentin@labelsublime.com>
2026-03-13 17:18:54 +0100

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,11 +1,13 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'data.json');
-  let data = { images: [] };
-  if (fs.existsSync(filePath)) {
-    data = JSON.parse(fs.readFileSync(filePath));
-  }
-  res.status(200).json(data);
+export default async function handler(req, res) {
+  const filePath = path.join(process.cwd(), "data/images.json");
+
+  let images = [];
+  try {
+    images = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  } catch(e){}
+
+  res.status(200).json({ images });
 }
